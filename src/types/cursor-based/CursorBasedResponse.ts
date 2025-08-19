0ba8019ea7eea44162917368f20c68715dcc7fea
Tomas Meriles <tomasmeriles17@gmail.com>
2025-08-19 13:10:07 -0300

    chore: remove unused response type files

--- src/types/cursor-based/CursorBasedResponse.ts
+++
@@ -1,3 +0,0 @@
-import type { cursorBasedResponse } from '../../responses/cursorBasedResponse.js';
-
-export type CursorBasedResponse = ReturnType<typeof cursorBasedResponse>;
--- src/types/offset-based/OffsetBasedResponse.ts
+++
@@ -1,3 +0,0 @@
-import type { offsetBasedResponse } from '../../responses/offsetBasedResponse.js';
-
-export type OffsetBasedResponse = ReturnType<typeof offsetBasedResponse>;
